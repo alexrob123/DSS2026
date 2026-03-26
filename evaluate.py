a4bf38d2b6878f0b736f4c598af3dfac17eed794
alexrob123 <alexandre.robin81@gmail.com>
2026-03-26 15:29:57 +0100

    eval subdir management for rattrapage

--- evaluate.py
+++ evaluate.py
@@ -88,23 +88,29 @@ def main():
 
 
 @main.command()
-@click.option(
-    "--num-exos",
-    "-n",
-    type=int,
-    default=5,
-    help="Number of exercises to evaluate.",
-)
-def tp_xml_dtd(num_exos=5):
+@click.option("--subdir", "-s",                 help="Subdir name in submission dir.",                  type=click.Path(exists=True, path_type=Path), default="tp-xml-dtd")  # fmt: skip
+@click.option("--num-exos", "-n",               help="Number of exercises to evaluate.",                type=int, default=5)  # fmt: skip
+@click.option("--out-csv", "-o",                 help="Output directory for evaluation results.",        type=click.Path(path_type=Path), default="eval_xml_dtd.csv")  # fmt: skip
+def tp_xml_dtd(subdir, num_exos, out_csv):
     """
     Evaluates student submissions for XML/DTD exercises.
-    The hw_dir should contain zipped folders for each student, each containing
-    their XML and DTD files for the exercises under format `exo_{i}.ext` (e.g.,
-    exo_1.xml, exo_1.dtd).
+    The submission/subdir directory should contain student submissions.
+
+    Required format is:
+    ```
+    NOM-DE-FAMILLE.zip
+    ├── exo_{i}.xml
+    └── exo_{i}.dtd
+    ```
+
+    Default usage:
+    ```
+    uv run evaluate.py tp-xml-dtd
+    ```
     """
 
-    input_dir = SUBMISSION_DIR / "tp-xml-dtd"
-    output_path = EVALUATION_DIR / "eval_xml_dtd.csv"
+    input_dir = SUBMISSION_DIR / subdir
+    output_path = EVALUATION_DIR / out_csv
 
     students = get_students(str(STUDENTS_CSV))
 
@@ -174,44 +180,35 @@ def tp_xml_dtd(num_exos=5):
 
 
 @main.command()
-@click.option(
-    "--num-exos",
-    "-n",
-    type=int,
-    default=9,
-    help="Number of exercises to evaluate.",
-)
-@click.option(
-    "--ref-script",
-    "--ref",
-    "-r",
-    "ref_script",
-    type=click.Path(exists=True, file_okay=True, path_type=Path),
-    help="Path to the reference implementation file (e.g., tp_dom_ref.py).",
-)
-@click.option(
-    "--xml-file",
-    "--test",
-    "-t",
-    "test_file",
-    type=click.Path(exists=True, file_okay=True, path_type=Path),
-    help="Path to the XML file used for testing student functions.",
-)
-def tp_dom(num_exos, ref_script, test_file):
+@click.option("--subdir", "-s",                 help="Subdir name in submission dir.",                  type=click.Path(exists=True, path_type=Path), default="tp-dom")  # fmt: skip
+@click.option("--num-exos", "-n",               help="Number of exercises to evaluate.",                type=int, default=9)  # fmt: skip
+@click.option("--ref-py", "--ref", "-r",    help="Path to py reference script (e.g., tp_dom.py).",  type=click.Path(exists=True, path_type=Path))  # fmt: skip
+@click.option("--test-xml", "--test", "-t",     help="Path to the XML test file (e.g., tp_dom.xml).",   type=click.Path(exists=True, path_type=Path))  # fmt: skip
+@click.option("--out-csv", "-o",                 help="Output directory for evaluation results.",        type=click.Path(path_type=Path), default="eval_dom.csv")  # fmt: skip
+def tp_dom(subdir, num_exos, ref_py, test_xml, out_csv):
     """
     Evaluates student submissions for DOM exercises.
-    The hw_dir should contain py folders for each student, each containing
-    their functions for the exercises under format `q{i}(xml_file)` (e.g.,
-    q1(xml_file)).
+    The submission/subdir directory should contain student submissions.
+
+    Required format is:
+    ```
+    NOM-DE-FAMILLE.py
+    └── q{i}(xml_file)
+    ```
+
+    Default usage:
+    ```
+    uv run evaluate.py tp-dom -r ./evaluation/utils/eval_dom.py -t ./evaluation/utils/eval_dom.xml
+    ```
     """
 
-    input_dir = SUBMISSION_DIR / "tp-dom"
-    output_path = EVALUATION_DIR / "eval_dom.csv"
+    input_dir = SUBMISSION_DIR / subdir
+    output_path = EVALUATION_DIR / out_csv
 
     students = get_students(str(STUDENTS_CSV))
 
     # Load reference answers
-    spec = importlib.util.spec_from_file_location("ref_module", ref_script)
+    spec = importlib.util.spec_from_file_location("ref_module", ref_py)
     ref_module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(ref_module)
 
@@ -220,9 +217,9 @@ def tp_dom(num_exos, ref_script, test_file):
         func_name = f"q{i}"
         if hasattr(ref_module, func_name):
             ref_func = getattr(ref_module, func_name)
-            ref_answers[i] = ref_func(str(test_file))
+            ref_answers[i] = ref_func(str(test_xml))
         else:
-            print(f"Reference function {func_name} not found in {ref_script}.")
+            print(f"Reference function {func_name} not found in {ref_py}.")
             ref_answers[i] = None
 
     # Evaluation loop
@@ -250,7 +247,7 @@ def tp_dom(num_exos, ref_script, test_file):
                 if hasattr(student_module, func_name):
                     student_func = getattr(student_module, func_name)
                     try:
-                        student_answer = student_func(str(test_file))
+                        student_answer = student_func(str(test_xml))
                         student_row[i] = 1 if student_answer == ref_answers[i] else 0
                         print(f"\t {func_name} OK: {student_answer == ref_answers[i]}")
                         if not student_answer == ref_answers[i]:
